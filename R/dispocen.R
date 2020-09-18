b11c52978b2ee9554186345c97b9e6a34555ac06
Jose Maria Sanchez Saez <jmss70@gmail.com>
2020-09-18 13:46:05 +0200

    Volcado inicial

---
+++ R/dispocen.R
@@ -0,0 +1,167 @@
+#' Zipf law
+zipf.law <- function(w, k=1,a=1,d=0) {
+  f = k/(seq_along(w)+d)^a
+  names(f) <- w
+  f
+}
+
+#' Exponential law
+exponential.law <- function(w,a=2,k=1,d=0) {
+  f = k/a^(seq_along(w)+d)
+  names(f) <- w
+  f
+}
+
+
+#' Additive reduce law
+additive.reduce.law <- function(w,k=1) {
+  1 - prod(1-w)^k
+}
+
+#' Read dispocen file
+#'
+#' This function reads file in the expected format from dispocen
+#' @param file File name of the file with the data
+#' @keywords read, dispocen
+read.dispocen <- function(file) {
+  f = readLines(file)
+  infos   = character()
+  users   = character()
+  centers = character()
+  words   = list()
+
+  for (i in seq_along(f)) {
+    a <- str_match(f[i], "^(\\d+) (\\d+) (\\d+) (.+)$")[-1]
+    infos[length(infos)+1] = a[1]
+    users[length(users)+1] = a[2]
+    centers[length(centers)+1] = a[3]
+    w = strsplit(a[4], ",")[[1]]
+    for (i in seq_along(w)) {
+      w[i] = trimws(w[i])
+    }
+    words[[length(words)+1]] = w
+  }
+  d <- data.frame(infos=infos, users=users, centers=centers, words=I(words))
+  d
+}
+
+# General function for obtaining the spectrum of a interest center
+# data   -> data.frame as provided by read.dispocen
+# law    -> law to quantify compatibility with interest center
+# reduce -> law to reduce the valorations of each word in each center to just one number
+# return a data.frame with the field availability added.
+build.availability <- function(data,law=exponential.law, reduce=additive.reduce.law) {
+  words        = character()
+  availability = numeric()
+
+  for (i in seq_along(data$words)) {
+    words        = c(words, data$words[i][[1]])
+    availability = c(availability, law(data$words[i][[1]]))
+  }
+  centers     = rep(data$centers, vapply(data$words, FUN = function(x) length(x), FUN.VALUE = 0L))
+
+  new.data <- data.frame(centers=centers, words=words, availability=availability) %>%
+    group_by(centers,words) %>%
+    summarise(availability=reduce(availability),freq.abs=length(words),.groups="drop")
+  total_by_center <- new.data %>%
+    group_by(centers) %>%
+    summarise(counttotal=sum(freq.abs), .groups="drop") %>% ungroup()
+  new.data <-
+    left_join(new.data, total_by_center, by="centers") %>%
+    mutate(freq.rel=freq.abs/counttotal) %>%
+    arrange(-freq.rel) %>%
+    group_by(centers) %>%
+    mutate(freq.abs.cum = cumsum(freq.abs), freq.rel.cum = cumsum(freq.rel)) %>%
+    ungroup() %>%
+    arrange(centers,-availability)
+  new.data$order <- unlist(tapply(new.data$words, new.data$centers,function(x) {seq_along(x)}))
+  new.data %>% select(centers,words,order,availability,freq.abs,freq.rel,freq.abs.cum,freq.rel.cum)
+}
+
+# Obtain the availability following the López-Strassburger model
+# return a data.frame with the field availability added.
+build.lopezstrass.availability <- function(data) {
+  # Máxima posición alcanzada
+  n <- max(vapply(data$words, function(x) {length(x)}, FUN.VALUE=1L))
+  # Número total de hablantes
+  N <- length(unique(data$users))
+
+  build.availability(data   = data,
+                     law    = function(w) {exponential.law(w,a=exp(2.3/(n-1)),d=-1)},
+                     reduce = function(x) {sum(x) / N})
+}
+
+# Obtain the availability following the Ávila-Sánchez model
+# returns a data.frame with the field availability added.
+build.avilasanchez.availability <- function(data, k=1) {
+  build.availability(data,
+                     law=function(x) {zipf.law(x,k=k,d=1)},
+                     reduce=additive.reduce.law)
+}
+
+# Sugeno integral of the fuzzy set d, respect fuzzy measure g
+# Returns the value of the integral
+
+fuzzy.expected.value <- function(d, g=function(x) {length(x)/length(d)}, h=function(x) {sqrt(x)}, level=0) {
+  ng <- function(x) {
+    res <- g(x)
+    for (i in seq(length.out=level)) {
+      res = h(res)
+    }
+    res
+  }
+
+  # Calculate by function, and the levels for alpha
+  levels <- sort(unique(d), decreasing=TRUE)
+  # Determine alpha cuts and its measure
+  gs <- sapply(levels,function(x) {ng(d[d >= x])})
+  res  <- cbind(levels,gs)
+  res <- max(apply(res,1,min))
+  res
+}
+
+# Label each word in each interest center in order to build levels of centrality
+# data -> Availability data.frame as returned by build.avilability
+# levels -> numbers of levels to clasify vocabulary
+# returns -> a new data.frame, similar to input, but with labeled levels
+classify.availability.levels <- function(data, levels=6) {
+  #new.data <- data %>% select(centers,words,order,availability)
+  new.data <- data
+  new.data$cutlevel = 0
+  new.data$level = 0
+  for (i in seq(length.out = levels)) {
+    cuts <- tapply(new.data$availability,new.data$centers,function(x) {fuzzy.expected.value(x,level=i)})
+    cuts <- data.frame(cuts=cuts,centers=names(cuts))
+    new.data <- left_join(new.data, cuts, by="centers")
+    new.data$level[new.data$availability >= new.data$cuts] <- i
+    new.data$cutlevel[new.data$availability >= new.data$cuts] <- new.data$cuts[new.data$availability >= new.data$cuts]
+    new.data <- new.data %>% select(centers,words,order,availability,level,cutlevel,freq.abs,freq.rel,freq.abs.cum,freq.rel.cum)
+  }
+  new.data %>% arrange(-availability,centers)
+}
+
+# Reduce data.frame to a
+build.availability.levels <- function(data) {
+  #mt <- tapply(data$words, list(data$centers,data$level),c,simplify=FALSE)
+  mt <- tapply(data$words, list(data$centers,data$level),c,simplify=FALSE)
+  dmt <- data.frame(mt)
+  names(dmt) <- colnames(mt)
+  dmt$centers <- rownames(dmt)
+  dmt <- dmt %>%
+    pivot_longer(-centers,names_to="level", values_to="words") %>%
+    mutate(level=strtoi(level))
+  dmt$count <- lapply(dmt$words, function(x) {length(unlist(x))}) %>% unlist
+  dmt$words <- lapply(dmt$words, function(x){paste(x,collapse=", ")})
+  dmt %>%
+    select(centers,level,count,words) %>%
+    arrange(centers,-level)
+}
+
+
+runUtility <- function() {
+  appDir <- system.file("utility", package="dispocen")
+  if (appDir=="") {
+    stop("Could not find utility directory. Try re-installing `dispocen`", .call=FALSE)
+  }
+  shiny::runApp(appDir, display.mode="normal")
+}
---
+++ inst/utility/app.R
@@ -0,0 +1,212 @@
+library(shiny)
+library(dispocen)
+library(tidyverse)
+
+models = data.frame(name=c("Lopez-Strassburger",
+                           "Avila-Sanchez (Zipf + Additive)"),
+                    funct=c('build.lopezstrass.availability',
+                               'build.avilasanchez.availability'))
+
+generateInform <- function(file, model) {
+  data <- read.dispocen(file$datapath)
+  centers <- data %>% select(centers) %>% arrange(centers) %>% unique() %>% pull()
+  model <- models %>% filter(name==model) %>% select(funct) %>% pull()
+  content <-
+    c('---',
+      'title: "Proyecto dispocen"',
+      'output:',
+      '  word_document: default',
+      '  html_notebook: default',
+      '  html_document:',
+      '    df_print: paged',
+      '  pdf_document: default',
+      'always_allow_html: yes',
+  '---',
+  '*Store this file in the same directory where your data is stored')
+
+  content <- c(content,
+               '# Carga de librerías',
+               '```{r message = FALSE, warning = FALSE}',
+               'library(dispocen)',
+               '```')
+  content <- c(content,
+               '# Carga de datos',
+               '```{r}',
+               paste('data <- read.dispocen("', file$name,'")', sep="", collapse=""),
+               'data %>% ',
+               '  head() %>% ',
+               '  flextable() %>% autofit() %>% ',
+               '  theme_booktabs()',
+               '```',
+               '')
+  content <- c(content,
+               '# Modelo de disponibilidad',
+               '```{r}',
+               paste('disponibilidad <- ', model, '(data)', sep="", collapse=""),
+               'disponibilidad %>% ',
+               '  head() %>% ',
+               '  flextable() %>% ',
+               '  set_header_labels(centers = "Centro de interés", ',
+               '                    words="Palabra", ',
+               '                    order="Orden", ',
+               '                    availability="Disponibilidad", ',
+               '                    freq.abs="Frecuencia absoluta", ',
+               '                    freq.abs.cum="Frecuencia absoluta acumulada", ',
+               '                    freq.rel="Frecuencia relativa", ',
+               '                    freq.rel.cum="Frecuencia relativa acumulada")  %>% ',
+               '  colformat_num(j=c(4,6,8), digits=6)  %>% ',
+               '  width(j=c(1,3,5,7),width=.65)  %>% ',
+               '  width(j=c(2,4,6,8),width=.9) %>% ',
+               '  theme_booktabs()',
+               '```',
+               '')
+  content <- c(content,
+               "## Centros de interés")
+
+  for (center in centers) {
+    content <- c(content,
+                 paste(c('### Centro de interés: ', center), sep="", collapse=""),
+                 '```{r eval=FALSE}',
+                 'disponibilidad %>% ',
+                 paste(c('  filter(centers=="', center, '") %>% '), sep="", collapse=""),
+                 '  arrange(-availability) %>% ',
+                 '  flextable() %>% ',
+                 '  set_header_labels(centers = "Centro de interés", ',
+                 '                    words="Palabra", ',
+                 '                    order="Orden", ',
+                 '                    availability="Disponibilidad", ',
+                 '                    freq.abs="Frecuencia absoluta", ',
+                 '                    freq.abs.cum="Frecuencia absoluta acumulada", ',
+                 '                    freq.rel="Frecuencia relativa", ',
+                 '                    freq.rel.cum="Frecuencia relativa acumulada")  %>% ',
+                 '  colformat_num(j=c(4,6,8), digits=6)  %>% ',
+                 '  width(j=c(1,3,5,7),width=.65)  %>% ',
+                 '  width(j=c(2,4,6,8),width=.9) %>% ',
+                 '  theme_booktabs()',
+                 '```',
+                '',
+                '```{r}',
+                'disponibilidad %>% ',
+                paste(c('  filter(centers=="', center, '") %>% '), sep="", collapse=""),
+                '  arrange(-availability) %>% ',
+                '  ggplot(aes(x=order, y=availability)) + geom_line() +',
+                '  xlab("Sucesión de palabras") + ylab("Disponibilidad")',
+                '```',
+                '')
+  }
+
+  content = c(content,
+              '## Visión general de los centros de interés',
+              '```{r}',
+              'disponibilidad %>%',
+              '  arrange(-availability) %>% ',
+              '  ggplot(aes(x=order,y=availability)) + geom_line() + facet_wrap(~centers)  +',
+              '  xlab("Secuencia de palabras (por grado descendente de compatibilidad)") + ',
+              '  ylab("Disponibilidad")',
+              '```',
+              '')
+  content = c(content,
+              '# Grupos de compatiblidad',
+              '```{r}',
+              'levels <- classify.availability.levels(disponibilidad)',
+              '```',
+              '```{r}',
+              'clasificacion <- build.availability.levels(levels)',
+              '```',
+              '')
+  content <- c(content,
+               "## Centros de interés")
+
+  for (center in centers) {
+    content <- c(content,
+                 paste(c('### Centro de interés: ', center), sep="", collapse=""),
+                 '```{r eval=FALSE}',
+                 'levels %>% ',
+                 '   arrange(-availability) %>%',
+                 '   select(-order) %>%',
+                 paste(c('  filter(centers=="', center, '") %>% '), sep="", collapse=""),
+                 '   flextable() %>% ',
+                 '   set_header_labels(centers = "Centro de interés", ',
+                 '                     words="Palabra", ',
+                 '                     availability="Disponibilidad",',
+                 '                     level="Nivel de disponibilidad",',
+                 '                     cutlevel="Nivel de corte",',
+                 '                     freq.abs="Frecuencia absoluta", ',
+                 '                     freq.abs.cum="Frecuencia absoluta acumulada", ',
+                 '                     freq.rel="Frecuencia relativa", ',
+                 '                     freq.rel.cum="Frecuencia relativa acumulada") %>% ',
+                 '   colformat_num(j=c(5,7,9), digits=5)  %>% ',
+                 '   width(j=c(1,4,6,8),width=.65)  %>% ',
+                 '   width(j=c(2,3,5,6,7,9),width=.75)  %>% ',
+                 '   theme_booktabs()',
+                 '```',
+                 ' ',
+                 '```{r}',
+                 'levels %>%',
+                 paste(c('  filter(centers=="', center, '") %>% '), sep="", collapse=""),
+                 '  mutate(level=factor(level)) %>% ',
+                 '  arrange(-availability) %>% ',
+                 '  ggplot(aes(x=order,y=availability,color=level)) + geom_line() +',
+                 '  xlab("Posición del término en el centro de interés") +',
+                 '  ylab("Disponibilidad")',
+                 '```',
+                 '',
+                 '```{r}',
+                 'clasificacion %>%',
+                 '  filter(level > 0) %>%',
+                 paste(c('  filter(centers=="', center, '") %>% '), sep="", collapse=""),
+                 '  flextable() %>% ',
+                 '  set_header_labels(centers = "Centro de interés", ',
+                 '                    words="Palabras",  ',
+                 '                    level="Nivel de disponibilidad", ',
+                 '                    count="Recuento") %>% ',
+                 '  width(j=c(1,2,3),width=.75) %>% ',
+                 '  width(j=4,width=4) %>% ',
+                 '  align(j=4,align="right") %>% ',
+                 '  theme_booktabs() ',
+                 '```',
+                 '```{r eval=FALSE}',
+                 'clasificacion %>% ',
+                 '  filter(level == 0) %>%',
+                 paste(c('  filter(centers=="', center, '") %>% '), sep="", collapse=""),
+                 '  flextable() %>% ',
+                 '  set_header_labels(centers = "Centro de interés", ',
+                 '                    words="Palabras",  ',
+                 '                    level="Nivel de disponibilidad", ',
+                 '                    count="Recuento") %>% ',
+                 '  width(j=c(1,2,3),width=.75) %>% ',
+                 '  width(j=4,width=4) %>% ',
+                 '  align(j=4,align="right") %>% ',
+                 '  theme_booktabs() ',
+                 '```')
+  }
+
+  paste(content, sep="", collapse="\n")
+
+}
+
+ui <- fluidPage(
+  titlePanel(h1("dispocen user interface")),
+  mainPanel(h2("File to process"),
+            fileInput("file",
+                      "Choose file to load"),
+            h2("Model to apply"),
+            radioButtons("model", "Model to apply",
+                         choices = list("Lopez-Strassburger",
+                                        "Avila-Sanchez (Zipf + Additive)")),
+            h2(""),
+            downloadButton("report", "Generate report"))
+)
+
+server <- function(input, output, session) {
+  output$report <- downloadHandler(
+    filename="report.rmd",
+    content = function(file) {
+      content = generateInform(input$file, input$model)
+      write(content, file=file)
+      stopApp()
+    }
+  )
+}
+
+shinyApp(ui = ui, server = server)
